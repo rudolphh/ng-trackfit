467030b42ce98d31ddb7b156f0856da7aebf76fb
Rudy A. Hernandez <rudolpharthur@gmail.com>
2021-06-09 15:26:23 -0700

    models - move directory (wrong directory)

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { User } from '../../_models/user';
+import { User } from '../_models/user';
 import { StorageService } from '../_services/storage.service';
 
 @Component({
