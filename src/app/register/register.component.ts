451bd38ff9592597c9940d8f20eca07fda6632e1
Rudy A. Hernandez <rudolpharthur@gmail.com>
2021-06-15 14:43:56 -0700

    register - aarons changes to regiser component

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { UserService } from '../_services/user.service';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  form!: FormGroup;
+  loading = false;
+  submitted = false;
 
-  constructor() { }
+
+
+
+  constructor(
+    private userService: UserService,
+    private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      username:['', Validators.required],
+      email:['', Validators.required],
+      password:['', Validators.required],
+      passwordConfirm:['', Validators.required]
+    })
   }
+    get f() {return this.form.controls}
 
+    onSubmit(){
+      this.submitted = true;
+
+      if(this.form.invalid){
+        return;
+      }
+
+      this.loading = true;
+      this.userService.register(this.form.value)
+      .pipe(first())
+      .subscribe({
+        next: () => {
+
+        }
+      })
+    }
 }
