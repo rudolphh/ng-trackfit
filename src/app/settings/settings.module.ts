9daadcd3ec5143ac89cae0153e9801f613011738
Gabe Ac <gabriel.ma.2288@gmail.com>
2021-06-23 16:56:50 -0700

    Finished Changes from Boostrap 4.5 to Material UI

--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -1,15 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SettingsComponent } from './settings/settings.component';
-
-
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field'; // Use instead of form tags ??
+import { MatButtonModule } from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
 
 @NgModule({
   declarations: [
     SettingsComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonToggleModule,
+    MatDatepickerModule,
+    MatIconModule,
+    MatSelectModule,
+    MatNativeDateModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatDividerModule
   ]
 })
 export class SettingsModule { }
--- src/app/settings/settings/settings.component.ts
+++ src/app/settings/settings/settings.component.ts
@@ -6,6 +6,11 @@ import { User } from 'src/app/_models/user';
 import { AuthService } from 'src/app/_services/auth.service';
 import { UserService } from 'src/app/_services/user.service';
 
+interface ReminderFrequency {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
@@ -15,6 +20,14 @@ export class SettingsComponent implements OnInit {
 
   currentUser : User = {};
   userSettings !: Settings;
+
+  reminderFrequencies: ReminderFrequency[] = [
+    {value: 'weekly', viewValue: 'Weekly'},
+    {value: 'bi-weekly', viewValue: 'Bi-Weekly'},
+    {value: 'monthly', viewValue: 'Monthly'},
+    {value: 'bi-monthly', viewValue: 'Bi-Monthly'},
+    {value: 'quarterly', viewValue: 'Quarterly'}
+  ];
 
   settingsForm = new FormGroup({
     gender : new FormControl(''),
