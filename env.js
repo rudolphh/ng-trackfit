649cc3604ade0f9ba49979baaca9c393ccf0539e
Rudy A. Hernandez <rudolpharthur@gmail.com>
2021-12-07 00:17:29 -0800

    Auto-generated commit [ci skip]

---
+++ env.js
@@ -0,0 +1,11 @@
+(function (window) {
+  window.__env = window.__env || {};
+
+  // API url
+  window.__env.apiUrl = 'https://fitapi.rudyah.com';
+  //window.__env.apiUrl = 'http://localhost:3838';
+
+  // Whether or not to enable debug mode
+  // Setting this to false will disable console output
+  window.__env.enableDebug = true;
+}(this));
