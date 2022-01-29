f315e5f9320addb30fc5c38b482a21dfb6f4e26d
Rudy A. Hernandez <rudolpharthur@gmail.com>
2022-01-28 23:47:25 -0800

    update

--- src/app/feature/home/dashboard/dashboard.component.ts
+++ src/app/feature/home/dashboard/dashboard.component.ts
@@ -1,6 +1,13 @@
-import { BehaviorSubject, Subject, Subscription, of } from 'rxjs';
+import {
+  BehaviorSubject,
+  Subject,
+  Subscription,
+  combineLatest,
+  forkJoin,
+  of,
+} from 'rxjs';
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { mergeMap, switchMap, takeUntil } from 'rxjs/operators';
+import { mergeMap, switchMap, take, takeUntil } from 'rxjs/operators';
 
 import { AuthService } from 'src/app/core/services/auth.service';
 import { Food } from 'src/app/core/models/food.model';
@@ -8,7 +15,8 @@ import { FoodDataService } from '../../food/services/food-data.service';
 import { HomeDataService } from '../home-data.service';
 import { Measurement } from 'src/app/core/models/measurement';
 import { MeasurementDataService } from '../../measurement/measurement-data.service';
-import { UserService } from '../../../core/services/user.service';
+import { SettingsDataService } from '../../settings/settings-data.service';
+import { UserSettings } from 'src/app/core/models/user-settings';
 
 declare var $: any;
 @Component({
@@ -19,17 +27,14 @@ declare var $: any;
 export class DashboardComponent implements OnDestroy {
   user$ = this.authService.currentUser;
   initialDate!: Date;
-
   bodyWeightSubject$ = new BehaviorSubject<number>(0);
   bodyWeight$ = this.bodyWeightSubject$.asObservable();
-
   bodyFatSubject$ = new BehaviorSubject<number>(0.0);
   bodyFat$ = this.bodyFatSubject$.asObservable();
-
-  foods !: Food[];
-
+  foods!: Food[];
   isLoading = false;
   inDemo = true;
+  userSettings!: UserSettings;
 
   unsubscribed$ = new Subject<void>();
 
@@ -38,13 +43,9 @@ export class DashboardComponent implements OnDestroy {
     private foodDataService: FoodDataService,
     private authService: AuthService,
     private measurementDataService: MeasurementDataService,
-    private userService: UserService
+    private settingsDataService: SettingsDataService
   ) {
-
     this.inDemo = this.authService.currentUserValue ? false : true;
-
-    // set initial date for entire dashboard
-    this.homeDataService.setCurrentDate(new Date());
 
     this.homeDataService.currentDate
       .pipe(
@@ -60,22 +61,45 @@ export class DashboardComponent implements OnDestroy {
       )
       .subscribe((foods) => {
         // every date change we need all other date related data changes to go here
-          this.isLoading = false;
+        this.isLoading = false;
       });
 
     this.foodDataService.todaysFood$.subscribe((foods) => {
       this.foods = foods;
     });
 
-    this.measurementDataService.measurements$.subscribe((measurements) => {
-      console.log(measurements);
-      if (measurements.length !== 0) {
-        this.bodyWeightSubject$.next(measurements[0].weight);
+    combineLatest([
+      this.settingsDataService.userSettings$,
+      this.measurementDataService.measurements$,
+    ])
+      .pipe(takeUntil(this.unsubscribed$))
+      .subscribe((data) => {
+        console.log(data);
+        const [userSettings, measurements] = data;
+        if (measurements.length !== 0) {
+          this.bodyWeightSubject$.next(measurements[0].weight);
+
+          const lastBodyMeasurements = measurements.find((m) => {
+            if (userSettings.gender === 'male') {
+              return m.neck && m.waist;
+            }
+            return m.neck && m.hips && m.waist;
+          });
+
+          console.log('lastBodyMeasurements', lastBodyMeasurements);
 
 
-        this.bodyFatSubject$.next(0.01);
-      }
-    });
+          if (lastBodyMeasurements) {
+            // can calculate bodyfat and use to better bmr * rate (cut or bulk)
+            // use Katch-McArdle
+            
+
+          } else {
+            // no last bodyfat calc, and use Mifflin-St.Jeor
+          }
+          this.bodyFatSubject$.next(0.01);
+        }
+      });
   }
 
   measurementAdded(measurement: Measurement): void {
