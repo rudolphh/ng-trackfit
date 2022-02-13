9cbb68159d8a797222af962145629c060304605c
Rudy A. Hernandez <rudolpharthur@gmail.com>
2022-02-13 12:46:45 -0800

    update daily macros

--- src/app/feature/home/dashboard/daily-macros/daily-macros.component.ts
+++ src/app/feature/home/dashboard/daily-macros/daily-macros.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 import { Food } from 'src/app/core/models/food.model';
 import { FoodDataService } from 'src/app/feature/food/services/food-data.service';
@@ -11,13 +12,20 @@ import { Observable } from 'rxjs';
 export class DailyMacrosComponent implements OnInit {
 
   @Input() foods$ !: Observable<Food[]>;
+  macroPercentForm !: FormGroup;
 
   protein = 0;
   carbohydrate = 0;
   fat = 0;
   max = { protein: 212, carbohydrate: 222, fat: 50 };
 
-  constructor(private foodDataService: FoodDataService) {}
+  constructor(private foodDataService: FoodDataService, private fb: FormBuilder) {
+    this.macroPercentForm = this.fb.group({
+      protein: [],
+      carbohydrate: [],
+      fat: []
+    });
+  }
 
   ngOnInit(): void {
     this.foods$.subscribe((foods) => {
